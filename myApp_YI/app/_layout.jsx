a18ac7daa867536feb391396a18f871fb8ea664a
EldrieYancyIcoy <bassilyoicoy.com@gmail.com>
2024-11-11 16:48:26 +0800

    First Commit

---
+++ myApp_YI/app/_layout.jsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+
+
+const MainLayout = () => {
+    return (
+        <Stack>
+            <Stack.Screen
+                name='index'
+                options={{headerShown:false}}
+            />
+        </Stack>
+    )
+}
+
+export default MainLayout
---
+++ myApp_YI/app/index.jsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome to KFC</Text>
+      <Link href="/profile">Go to Profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
